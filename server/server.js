981630da46f6f624b829761fd57adcf5e42bb4f0
ashkan <haghgo1374.ashkan@gmail.com>
2019-05-25 22:42:58 +0430

    adding server,socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-const app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+    console.log("New user connected !");
+
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected');
+    })
+});
 
 if (!module.parent) {
-    app.listen(3000, () => {
-        console.log(`Chat app is running on port ${port} !`);
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port} !`);
     });
 }
